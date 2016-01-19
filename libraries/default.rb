4ebe330fa3665e72ecda0575ff1b75a76bb9d170
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2016-01-19 18:15:59 +0400

    Fix bugs

--- libraries/default.rb
+++ libraries/default.rb
@@ -4,12 +4,28 @@ module ModernNginx
       ::File.join node['nginx']['dir'], 'cert'
     end
 
+    def get_ssl_certificate_entry(domain)
+      data_bag_item(node[:modern_nginx][:ssl_data_bag_name], node.chef_environment).to_hash.fetch('certs', []).detect do |entry|
+        entry['domains'].include? domain
+      end
+    end
+
     def get_ssl_certificate_path(domain)
-      ::File.join get_ssl_cert_directory, "#{domain}.chain.crt"
+      entry = get_ssl_certificate_entry domain
+      if entry.nil?
+        nil
+      else
+        ::File.join get_ssl_cert_directory, "#{entry['domains'][0]}.chain.crt"
+      end
     end
 
     def get_ssl_certificate_private_key_path(domain)
-      ::File.join get_ssl_cert_directory, "#{domain}.key"
+      entry = get_ssl_certificate_entry domain
+      if entry.nil?
+        nil
+      else
+        ::File.join get_ssl_cert_directory, "#{entry['domains'][0]}.key"
+      end
     end
 
     def get_scts_base_directory
@@ -26,12 +42,6 @@ module ModernNginx
 
     def get_acme_challenge_directory(domain)
       ::File.join get_acme_challenge_base_directory, domain
-    end
-
-    def get_ssl_certificate_entry(domain)
-      data_bag_item(node[:modern_nginx][:ssl_data_bag_name], node.chef_environment).to_hash.fetch('certs', []).detect do |entry|
-        entry['domains'].include? domain
-      end
     end
 
     def get_hpkp_pins(domain)
--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name 'modern_nginx'
 description 'Installs and configures nginx'
-version '1.2.0'
+version '1.2.1'
 
 recipe 'modern_nginx', 'Installs and configures nginx'
 depends 'nginx', '2.7.6'
