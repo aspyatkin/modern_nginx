630b9bf9344154d539d9ba3ec5ba5bd11b464eb1
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2017-03-29 08:48:48 +0400

    Update SSL ciphers

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name 'modern_nginx'
 description 'Installs and configures nginx'
-version '2.1.1'
+version '2.2.0'
 
 recipe 'modern_nginx', 'Installs and configures nginx'
-depends 'chef_nginx', '~> 5.1.2'
+depends 'chef_nginx', '~> 6.0.0'
 depends 'dhparam', '~> 1.0.0'
