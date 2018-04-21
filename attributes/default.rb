fdde01bbf6da6e9ceb3c67b96e4e305d42341e84
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2018-04-21 18:54:18 +0400

    Update versions

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,13 +2,13 @@ id = 'modern_nginx'
 
 default[id]['install'] = 'stable'
 
-default[id]['stable']['version'] = '1.12.2'
+default[id]['stable']['version'] = '1.14.0'
 default[id]['stable']['checksum'] = \
-  '305f379da1d5fb5aefa79e45c829852ca6983c7cd2a79328f8e084a324cf0416'
+  '5d15becbf69aba1fe33f8d416d97edd95ea8919ea9ac519eff9bafebb6022cb5'
 
-default[id]['mainline']['version'] = '1.13.9'
+default[id]['mainline']['version'] = '1.13.12'
 default[id]['mainline']['checksum'] = \
-  '5faea18857516fe68d30be39c3032bd22ed9cf85e1a6fdf32e3721d96ff7fa42'
+  'fb92f5602cdb8d3ab1ad47dbeca151b185d62eedb67d347bbe9d79c1438c85de'
 
 default[id]['with_openssl'] = true
 default[id]['with_http2'] = true
@@ -21,9 +21,9 @@ default[id]['with_geoip2'] = false
 default[id]['with_secure_link'] = false
 default[id]['with_debug'] = false
 
-default[id]['openssl']['version'] = '1.1.0g'
+default[id]['openssl']['version'] = '1.1.0h'
 default[id]['openssl']['checksum'] = \
-  'de4d501267da39310905cb6dc8c6121f7a2cad45a7707f76df828fe1b85073af'
+  '5835626cde9e99656585fc7aaa2302a73a7e1340bf8c14fd635a62c66802a517'
 
 default[id]['ct']['version'] = '1.3.2'
 default[id]['ct']['url'] = 'https://github.com/grahamedgecombe/nginx-ct/archive'
@@ -47,12 +47,12 @@ default[id]['geoip2']['databases']['country']['name'] = 'GeoLite2-Country.mmdb'
 default[id]['geoip2']['databases']['country']['url'] = \
   'http://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.tar.gz'
 default[id]['geoip2']['databases']['country']['checksum'] = \
-  '7f4b455ba611b623ae9f45afe5249aae9f881befd4f78489d21dee6f94faf1fa'
+  '1a002b9321244c689ee20a46c1b368e2bf2e3ce5f29148de38820891b5db17d5'
 
 default[id]['geoip2']['databases']['city']['name'] = 'GeoLite2-City.mmdb'
 default[id]['geoip2']['databases']['city']['url'] = \
   'http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.tar.gz'
 default[id]['geoip2']['databases']['city']['checksum'] = \
-  '82400c9519f8c3d90b7b4eea451053c55efbc939d9de69f1fbc3bd443be652ed'
+  'c86219316fbadccfe3973330e3946ce14bd57dbadafdb5e7c0f526c5bb27a933'
 
 default[id]['ssl_configuration'] = 'modern' # or intermediate
--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
 name 'modern_nginx'
 description 'Installs and configures nginx'
-version '2.11.0'
+version '2.12.0'
 
 recipe 'modern_nginx', 'Installs and configures nginx'
-depends 'nginx', '~> 8.1.2'
+depends 'nginx'
 depends 'dhparam', '~> 1.0.0'
 depends 'logrotate', '~> 2.2.0'
